2a8d67d482fb604ae33d009a917287435a1f523e
HaoHao <9203700@gmail.com>
2017-05-21 15:30:53 +0800

    Add prototype.

---
+++ algo_hw3.hpp
@@ -0,0 +1,25 @@
+
+// change this to your id
+static const char* student_id = "3345678" ;
+
+
+// do not edit prototype
+void LCS(int *, int*, int*);
+
+// X,Y are input strings, C is answer string
+//
+// data structure :  
+// length of array X is m+1, length of array Y is n+1, length of array C is m+n
+// X[0] = m+1, Y[0] = n+1, 
+// all element of C are "-1"
+// other datas are in [0,255] means the symble in ASCII or ANSI table
+
+
+// you only allow declare a table with size (m+1)*(n+1)
+//
+// do your homework here
+//
+
+void LCS(int* X, int* Y, int* C) {
+
+}
