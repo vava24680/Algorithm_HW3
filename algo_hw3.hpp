2d77321838af4a961a66f78f9a93572b34a1bce9
HaoHao <9203700@gmail.com>
2017-05-21 15:56:38 +0800

    Add lcs_length_table

--- algo_hw3.hpp
+++ algo_hw3.hpp
@@ -1,6 +1,6 @@
 
 // change this to your id
-static const char* student_id = "3345678" ;
+static const char* student_id = "0416005" ;
 
 
 // do not edit prototype
@@ -8,9 +8,9 @@ void LCS(int *, int*, int*);
 
 // X,Y are input strings, C is answer string
 //
-// data structure :  
+// data structure :
 // length of array X is m+1, length of array Y is n+1, length of array C is m+n
-// X[0] = m+1, Y[0] = n+1, 
+// X[0] = m+1, Y[0] = n+1,
 // all element of C are "-1"
 // other datas are in [0,255] means the symble in ASCII or ANSI table
 
@@ -20,6 +20,36 @@ void LCS(int *, int*, int*);
 // do your homework here
 //
 
-void LCS(int* X, int* Y, int* C) {
+/*
+	lcs_length_table will be like this
+	**************
+	**************
+	**************
+	**************
+	**************
+	row size:first_size
+	col size:second_size
+*/
 
+
+void LCS(int* X, int* Y, int* C)
+{
+	int first_size = X[0];
+	int second_size = Y[0];
+	int** lcs_length_table;
+	lcs_length_table = new int*[first_size];
+	for(int i=0;i<first_size;i++)
+	{
+		lcs_length_table[i] = new int[second_size];
+	}
+	//set first row of lcs_length_table to zero
+	for(int j=0;j<second_size;j++)
+	{
+		lcs_length_table[0][j] = 0;
+	}
+	//set first column of lcs_length_table to zero
+	for(int i=0;i<first_size;i++)
+	{
+		lcs_length_table[i][0] = 0;
+	}
 }
