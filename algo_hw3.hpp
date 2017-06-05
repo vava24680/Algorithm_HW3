0d71da69181da8d0081d2567f41574cdb637d6f8
vava24680 <9203700@gmail.com>
2017-06-05 21:11:13 +0800

    Finished

--- algo_hw3.hpp
+++ algo_hw3.hpp
@@ -1,4 +1,7 @@
-
+#include<string>
+#include<iostream>
+#include<cstdio>
+using namespace std;
 // change this to your id
 static const char* student_id = "0416005" ;
 
@@ -27,38 +30,43 @@ void LCS(int *, int*, int*);
 	**************
 	**************
 	**************
-	row size:first_size
-	col size:second_size
+	row size:row_size
+	col size:col_size
 */
 
-
+void extract_LCS(const int* first_string,const int* second_string, int* dst_string, int** lcs_length_table, int row_index, int col_index, int dst_index);
 void LCS(int* X, int* Y, int* C)
 {
 	int* first_string = X;
 	int* second_string = Y;
-	int first_size = X[0];//First string length
-	int second_size = Y[0];//Secode string length
+	int* temp_dst_string = 0;
+	int lcs_max_length = (first_string[0]-1<=second_string[0]-1) ? first_string[0]-1 : second_string[0]-1 ;
+	int row_size = X[0];//First string length+1
+	int col_size = Y[0];//Secode string length+1
+	/*In the computing lcs_table, the first row and first column is preserved but not used, so our size must be string length plus 1*/
 	int** lcs_length_table;
-	lcs_length_table = new int*[first_size];
-	for(int i=0;i<first_size;i++)
+	lcs_length_table = new int*[row_size];
+	for(int i=0;i<row_size;i++)
 	{
-		lcs_length_table[i] = new int[second_size];
+		lcs_length_table[i] = new int[col_size];
 	}
 	//set first row of lcs_length_table to zero
-	for(int j=0;j<second_size;j++)
+	for(int j=0;j<col_size;j++)
 	{
 		lcs_length_table[0][j] = 0;
 	}
 	//set first column of lcs_length_table to zero
-	for(int i=0;i<first_size;i++)
+	for(int i=0;i<row_size;i++)
 	{
 		lcs_length_table[i][0] = 0;
 	}
-	for(int i=1;i<=first_size;i++)
+	/*First string is at position 1 to row_size-1
+	  Second string is at position 1 to col_size-1*/
+	for(int i=1;i<row_size;i++)
 	{
-		for(int j=1;j<=second_size;j++)
+		for(int j=1;j<col_size;j++)
 		{
-			if(first_string[i]==second_string[i])
+			if(first_string[i]==second_string[j])
 			{
 				lcs_length_table[i][j] = lcs_length_table[i-1][j-1]+1;
 				//NO direction record
@@ -69,28 +77,60 @@ void LCS(int* X, int* Y, int* C)
 			}
 			else
 			{//lcs_length_table[i][j-1] is larger
-				lcs_length_table[i][j] = lcs_length_table[j][i-1];
+				lcs_length_table[i][j] = lcs_length_table[i][j-1];
 			}
 		}
 	}
+	/*
+	Temparay array to store the result of LCS
+	Since I don't know how long the LCS is, I use a temparay array to store the result of LCS
+	This temparay array length is the biggest length among two original strings
+	And whole array initial value is -1 so I can distinguish where is the start of LCS after extract_LCS executed
+	*/
+	temp_dst_string = new int[lcs_max_length];
+	for(int i=0;i<lcs_max_length;i++)
+	{
+		temp_dst_string[i] = -1;
+	}
+	/*The last three parameters have minus 1 is because of they represent the index of 0-based array*/
+	extract_LCS(first_string,second_string,temp_dst_string,lcs_length_table,first_string[0]-1,second_string[0]-1,lcs_max_length-1);
+	for(int i=0,j=0;i<lcs_max_length;i++)
+	{
+		if(temp_dst_string[i]!=-1)
+		{
+			C[j] = temp_dst_string[i];//Copy the result to C array
+			j+=1;
+		}
+		else
+			continue;
+	}
+	if(temp_dst_string!=0)
+		delete [] temp_dst_string;
+	for(int i=0;i<row_size;i++)
+	{
+		if(lcs_length_table[i]!=0)
+			delete [] lcs_length_table[i];
+	}
+	if(lcs_length_table!=0)
+		delete [] lcs_length_table;
 }
 
-/*void extract_LCS(int* first_string, int* dst_string, int** lcs_length_table, int row_index, int col_index)
+void extract_LCS(const int* first_string, const int* second_string,int* dst_string,int** lcs_length_table, int row_index, int col_index, int dst_index)
 {
 	if(row_index==0 || col_index==0)
+	//Terminal condition for recursive func. extract_LCS
 		return;
-	if(lcs_length_table[row_index][col_index]==lcs_length_table[row_index-1][col_index])
-	{
-		extract_LCS(first_string,lcs_length_table,row_index-1,col_index);
+	if(lcs_length_table[row_index][col_index]==lcs_length_table[row_index-1][col_index-1]+1 && first_string[row_index]==second_string[col_index])
+	{//Equivalent to the "To left-up" move in b table
+		extract_LCS(first_string,second_string,dst_string,lcs_length_table,row_index-1,col_index-1,dst_index-1);
+		dst_string[dst_index]=first_string[row_index];
 	}
-	else if(lcs_length_table[row_index][col_index]==lcs_length_table[row_index][col_index-1])
-	{
-		extract_LCS(first_string,lcs_length_table,row_index,col_index-1);
+	else if(lcs_length_table[row_index][col_index]==lcs_length_table[row_index-1][col_index])
+	{//Equivalent to the "To up" move in b table
+		extract_LCS(first_string,second_string,dst_string,lcs_length_table,row_index-1,col_index,dst_index);
 	}
 	else
-	{//Only extract the character out onlf if first_string[row_index]==second_string[col_index]
-	 //Or it will not be called as Longest Common sequence.
-		extract_LCS(first_string,lcs_length_table,row_index-1,col_index-1);
-		dst_string[row_index] = first_string[row_index];
+	{//Equivalent to the "To left" move in b table
+		extract_LCS(first_string,second_string,dst_string,lcs_length_table,row_index,col_index-1,dst_index);
 	}
-}*/
+}
